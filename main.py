c660e0ff139d5b9fd227e671f67535d425460b51
Trent <binarytrent@proton.me>
2023-12-07 09:21:00 -0600

    Api fully works now.

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from flask import Flask
+import requests
+
+# Meme_api at work
+def get_meme():
+    """This is the meme api that adds 'fun' into my programming"""
+    api_url = "https://meme-api.com/gimme"
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        data = response.json()
+        return f"{data['postLink']}"
+    else:
+        return "Failed to get a reddit request, code no worky LOL"
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>If you see this, flask works :D</p>"
+    return "<p>Hello, World!</p>"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    print(get_meme())
