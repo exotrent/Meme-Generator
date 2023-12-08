ffae554b10fa4299863512e59289a88f7903739b
Trent <binarytrent@proton.me>
2023-12-08 09:32:02 -0600

    uppdated and formatted code

--- main.py
+++ main.py
@@ -9,17 +9,29 @@ def get_meme():
 
     if response.status_code == 200:
         data = response.json()
-        return f"{data['preview'] [2]}"
+        return f"{data['preview'] [3]}"
+    else:
+        return "Failed to get a reddit request, code no worky LOL"
+    
+def get_author():
+    """This is the meme api that adds 'fun' into my programming"""
+    api_url = "https://meme-api.com/gimme"
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        data = response.json()
+        return f"{data['author']}"
     else:
         return "Failed to get a reddit request, code no worky LOL"
 
 # Vars    
 app = Flask(__name__)
 web_var = get_meme()
+author = get_author()
 
 @app.route("/")
 def meme_generator():
-    return render_template('index.html', web_var=web_var)
+    return render_template('index.html', web_var=web_var, author=author)
 
 
 if __name__ == '__main__':
